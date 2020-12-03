dbac0282bda6bd44f05920f2b00f01b7971f4db1
Patryk Jesionka <patryk@UNKNOWN.Home>
2020-12-03 18:40:04 +0000

    Corrected for some errors with mapping.

--- app/website/urls.py
+++ app/website/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/project/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail'),
-    path('/hire-me/', views.hire_me, name='hire-me'),
-    path('/upload-image/', views.upload_image, name='upload-image'),
+    path('project/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail'),
+    path('hire-me/', views.hire_me, name='hire-me'),
+    path('image-upload/', views.image_upload, name='image-upload'),
 ]

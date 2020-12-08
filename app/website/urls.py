9e06adf64f59d483a756442cdb4a76ceecb0569b
Patryk Jesionka <patryk@UNKNOWN.Home>
2020-12-08 15:38:42 +0000

    References Index view as generic list view

--- app/website/urls.py
+++ app/website/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
     path('project/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail'),
     path('hire-me/', views.hire_me, name='hire-me'),
-    path('image-upload/', views.image_upload, name='image-upload'),
+    path('upload/', views.upload, name='upload'),
 ]

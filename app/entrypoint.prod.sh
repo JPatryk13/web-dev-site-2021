7b7b077b180605f3a38dccae1e6d58c03e27c352
Patryk Jesionka <patryk@UNKNOWN.Home>
2021-01-03 20:26:26 +0000

    added seeding database and comments - now it looks similar to the dev entrypoint

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -11,8 +11,15 @@ then
     echo "PostgreSQL started"
 fi
 
+# Migrate
 python manage.py makemigrations --noinput
 python manage.py migrate
+
+# Seed database with fake data
+python manage.py seed --table Project --entries 4
+python manage.py seed --table Link --entries 18
+
+# Create superuser and collect static files
 python manage.py createsu
 python manage.py collectstatic --noinput
 

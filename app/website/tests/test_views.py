3c47ecf99a9aace54ecbd26ff1ba81e0875855c1
Patryk Jesionka <patryk@UNKNOWN.Home>
2021-01-13 15:23:41 +0000

    Initial commit of the views test

---
+++ app/website/tests/test_views.py
@@ -0,0 +1,70 @@
+from django.test import TestCase
+from .mixins.ViewTestMixin import ViewTestMixin
+from .mixins.ModelTestMixin import ModelTestMixin
+import random
+from faker import Faker
+
+from website import views
+
+
+class IndexTest(TestCase, ViewTestMixin):
+    view_class = views.Index
+
+    def test_get_request(self):
+        self.is_callable()
+
+    def test_post_request(self):
+        self.is_callable(
+            post=True,
+            to='contact-success',
+            data={
+                'name': 'John Smith',
+                'email': 'jsmith@gmail.com',
+                'message': 'Hi! How things are going?'
+            }
+        )
+
+
+class ProjectDetailViewTest(TestCase, ViewTestMixin, ModelTestMixin):
+    view_class = views.ProjectDetailView
+
+    def setUp(self):
+        self.proj = self.create_project()
+
+    def test_get_request(self):
+        self.is_callable(kwargs={'pk': self.proj.pk})
+
+
+class HireMeTest(TestCase, ViewTestMixin):
+    view_class = views.HireMe
+
+    def setUp(self):
+        self.faker = Faker('en_US')
+
+    def test_get_request(self):
+        self.is_callable()
+
+    def test_post_request(self):
+        self.is_callable(
+            post=True,
+            to='hire-me-success',
+            data={
+                'name': 'John Smith',
+                'email': 'jsmith@gmail.com',
+                'project_type': random.choice(['d', 'b', 'f', 's']),
+                'project_status': random.choice(['e', 'n']),
+                'description': self.faker.text(max_nb_chars=500)
+            }
+        )
+
+
+class UploadTest(TestCase):
+
+    def test_anonymous_user(self):
+        pass
+
+    def test_get_request(self):
+        pass
+
+    def test_post_request(self):
+        pass
